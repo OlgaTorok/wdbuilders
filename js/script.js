c009918e80091d4c28099ba2fceee97b69ab230a
Olga Torok <olgaetorok@gmail.com>
2021-05-11 20:19:56 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+$(function() {
+
+  let wheight = $(window).height(); //get the height of the window
+  $('.fullheight').css('height', wheight); //set to window tallness
+
+  //replace IMG inside carousels with a background image
+  $('#home .carousel-item img').each(function() {
+      var imgSrc = $(this).attr('src');
+      $(this).parent().css({'background-image': 'url('+imgSrc+')'});
+      $(this).remove();
+  });
+
+  //adjust height of .fullheight elements on window resize
+  $(window).resize(function() {
+      wheight = $(window).height(); //get the height of the window
+      $('.fullheight').css('height', wheight); //set to window tallness
+  });
+});
